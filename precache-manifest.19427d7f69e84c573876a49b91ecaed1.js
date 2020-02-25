22353ce45376b1a549061e2bfea4612028dc2759
Mateus Andrade <mat.andrade@live.com>
2020-02-25 11:14:45 -0300

    Updates

---
+++ precache-manifest.19427d7f69e84c573876a49b91ecaed1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34051501b7c4d1ad7818eeee11c2b4cf",
+    "url": "/rocketseat-rocketshoes/index.html"
+  },
+  {
+    "revision": "4a8e8f38e493efa4764f",
+    "url": "/rocketseat-rocketshoes/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "4a8e8f38e493efa4764f",
+    "url": "/rocketseat-rocketshoes/static/js/2.54f377b5.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/rocketseat-rocketshoes/static/js/2.54f377b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96d35c8807b45c15efa",
+    "url": "/rocketseat-rocketshoes/static/js/main.3111f305.chunk.js"
+  },
+  {
+    "revision": "c41c55c3787f39bdce61",
+    "url": "/rocketseat-rocketshoes/static/js/runtime-main.ad958cfa.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/rocketseat-rocketshoes/static/media/bg.3c9a1ce5.svg"
+  }
+]);
