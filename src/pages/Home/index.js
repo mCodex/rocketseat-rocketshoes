3600c914f3abdff4ea10069a78d0a63565acc902
Mateus Andrade <mat.andrade@live.com>
2020-03-03 20:21:05 -0300

    added products loaded from API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,25 +1,55 @@
-import React, { memo, useState, useEffect } from 'react';
+import React, { memo, useState, useEffect, useCallback } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 
-import shoeImg from '~/assets/images/shoe.jpg';
+import moneyFormatter from '~/lib/moneyFormatter';
+
+import api from '~/services/api';
 
 import { ProductList } from './styles';
 
+type Product = {
+    id: Number,
+    title: String,
+    price: Number,
+    image: String,
+};
+
 const Home = () => {
+    const [products, setProducts] = useState([]);
+
+    const loadDataFromAPI = useCallback(async () => {
+        const productsFromAPI = await api.get('products');
+
+        const data = productsFromAPI.data.map(product => ({
+            ...product,
+            price: moneyFormatter.format(product.price),
+        }));
+
+        setProducts(data);
+    }, []);
+
+    useEffect(() => {
+        loadDataFromAPI();
+    }, [loadDataFromAPI]);
+
+    const renderProductItem = ({ id, title, price, image }: Product) => (
+        <li key={id.toString()}>
+            <img src={image} alt="Shoe" />
+
+            <strong>{title}</strong>
+            <span>{price}</span>
+            <button type="submit">
+                <div>
+                    <MdAddShoppingCart size={16} color="#fff" />
+                </div>
+                <span>ADICIONAR AO CARRINHO</span>
+            </button>
+        </li>
+    );
+
     return (
         <ProductList>
-            <li>
-                <img src={shoeImg} alt="Shoe" />
-
-                <strong>Tênis</strong>
-                <span>R$</span>
-                <button type="submit">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" />
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
+            {products.map(product => renderProductItem(product))}
         </ProductList>
     );
 };
