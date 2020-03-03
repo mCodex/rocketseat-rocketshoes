30c65589c1637dc22917bee0d97611f4b0a059e0
Mateus Andrade <mat.andrade@live.com>
2020-03-02 21:11:44 -0300

    added my json server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL:
+        'https://my-json-server.typicode.com/mcodex/rocketseat-rocketshoes/',
 });
 
 export default api;
