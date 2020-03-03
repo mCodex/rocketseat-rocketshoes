b8714beea7d02aa2e669889bf5f861d19e792505
Mateus Andrade <mat.andrade@live.com>
2020-03-03 20:25:50 -0300

    added redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { toast } from 'react-toastify';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Routes from './routes';
 
@@ -9,14 +10,18 @@ import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import 'react-toastify/dist/ReactToastify.css';
 
+import store from './store';
+
 toast.configure();
 
 const App = () => (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Header />
-        <Routes />
-        <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <Header />
+            <Routes />
+            <GlobalStyle />
+        </BrowserRouter>
+    </Provider>
 );
 
 export default App;
