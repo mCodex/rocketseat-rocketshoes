d7302e82b2b9a42514ba1127bf37c1143825b5d2
Mateus Andrade <mat.andrade@live.com>
2020-03-08 09:50:54 -0300

    Updates

--- precache-manifest.4b88ff890ec0c595bf2e623088271eb1.js
+++ precache-manifest.4b88ff890ec0c595bf2e623088271eb1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "519770b15612f512aa765cecba595e41",
+    "revision": "0866a6dc69b928fbd2b6085513f6bfb1",
     "url": "/rocketseat-rocketshoes/index.html"
   },
   {
-    "revision": "ee229130a2c3bd7bd68b",
+    "revision": "a2a4c13fba2b8817dda5",
     "url": "/rocketseat-rocketshoes/static/css/2.5dbdccff.chunk.css"
   },
   {
-    "revision": "ee229130a2c3bd7bd68b",
-    "url": "/rocketseat-rocketshoes/static/js/2.6c6c1f39.chunk.js"
+    "revision": "a2a4c13fba2b8817dda5",
+    "url": "/rocketseat-rocketshoes/static/js/2.d28483bc.chunk.js"
   },
   {
     "revision": "d48f557b335fe40c661607e7b46704c8",
-    "url": "/rocketseat-rocketshoes/static/js/2.6c6c1f39.chunk.js.LICENSE.txt"
+    "url": "/rocketseat-rocketshoes/static/js/2.d28483bc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "579eed0e77adb3bc19af",
-    "url": "/rocketseat-rocketshoes/static/js/main.23556632.chunk.js"
+    "revision": "c6832ded7f856843554c",
+    "url": "/rocketseat-rocketshoes/static/js/main.12564ccd.chunk.js"
   },
   {
     "revision": "c41c55c3787f39bdce61",
